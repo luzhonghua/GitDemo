5dd6214f7bcc10b58d43e97ef2b2482892a57934
louhugh <1305525581@qq.com>
2016-07-19 15:55:33 +0800

    哈哈04

--- app/src/main/java/mydemo/com/loughu/cn/gitdemo/MainActivity.java
+++ app/src/main/java/mydemo/com/loughu/cn/gitdemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("haha");
         System.out.println("haha01");
+        System.out.println("haha02");
     }
 }
